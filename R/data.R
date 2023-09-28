7c0ec4161447c333e6e81b8c1fc125e163c18bcc
PJ Dodd <p.j.dodd@sheffield.ac.uk>
2023-09-28 07:11:16 +0100

    documenting data

---
+++ R/data.R
@@ -0,0 +1,9 @@
+#' Model Parameters
+#'
+#' A default set of parameter values for running the model
+#' 
+#'
+#' @format ## `parms`
+#' A list in the right format for passing to `runmodel`
+#' @source Created by code inst/extdata/parameters.R
+"parms"
