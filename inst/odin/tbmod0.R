6b644c513c4f8561beb65960416e79de298d744e
PJ Dodd <p.j.dodd@sheffield.ac.uk>
2023-12-14 10:59:39 +0000

    including screening flows

--- inst/odin/tbmod0.R
+++ inst/odin/tbmod0.R
@@ -23,6 +23,11 @@ att_time <- user(0.5)     #duration of ATT
 late_post_time <- user(2) #duration defining early post-TB
 mort <- user(0.02)        #mortality rate
 
+## economic parameters
+disc_rate <- user(0.03)
+
+
+
 ## PPD transitions
 inflow <- user()          # inflow rate (less recidivists)
 remand_short <- user()    # remand -> short:   1->2
@@ -65,14 +70,21 @@ parm_ifrac_prevTPT[] <- user() #TPT state
 parm_init_PPD[] <- user()
 
 ################## initial state
-initial(U[1:NP,1:NT]) <- parm_ifrac_U * parm_init_PPD[i] * parm_ifrac_prevTPT[j] #uninfected
-initial(E[1:NP,1:NT]) <- parm_ifrac_E * parm_init_PPD[i] * parm_ifrac_prevTPT[j] #early latent
-initial(L[1:NP,1:NT]) <- parm_ifrac_L * parm_init_PPD[i] * parm_ifrac_prevTPT[j] #late latent
-initial(SD[1:NP,1:NT]) <- parm_ifrac_SD * parm_init_PPD[i] * parm_ifrac_prevTPT[j] #subclinical disease
-initial(CD[1:NP,1:NT]) <- parm_ifrac_CD * parm_init_PPD[i] * parm_ifrac_prevTPT[j] #clinical disease
-initial(ATT[1:NP,1:NT]) <- parm_ifrac_ATT * parm_init_PPD[i] * parm_ifrac_prevTPT[j] #AntiTB treatment
-initial(epTB[1:NP,1:NT]) <- parm_ifrac_epTB * parm_init_PPD[i] * parm_ifrac_prevTPT[j] #early post-TB
-initial(lpTB[1:NP,1:NT]) <- parm_ifrac_lpTB * parm_init_PPD[i] * parm_ifrac_prevTPT[j] #late post-TB
+initial(U[1:NP,1:NT]) <- parm_ifrac_U * parm_init_PPD[i] * parm_ifrac_prevTPT[j] ##uninfected
+initial(E[1:NP,1:NT]) <- parm_ifrac_E * parm_init_PPD[i] * parm_ifrac_prevTPT[j] ##early latent
+initial(L[1:NP,1:NT]) <- parm_ifrac_L * parm_init_PPD[i] * parm_ifrac_prevTPT[j] ##late latent
+initial(SD[1:NP,1:NT]) <- parm_ifrac_SD * parm_init_PPD[i] * parm_ifrac_prevTPT[j] ##subclinical disease
+initial(CD[1:NP,1:NT]) <- parm_ifrac_CD * parm_init_PPD[i] * parm_ifrac_prevTPT[j] ##clinical disease
+initial(ATT[1:NP,1:NT]) <- parm_ifrac_ATT * parm_init_PPD[i] * parm_ifrac_prevTPT[j] ##AntiTB treatment
+initial(epTB[1:NP,1:NT]) <- parm_ifrac_epTB * parm_init_PPD[i] * parm_ifrac_prevTPT[j] ##early post-TB
+initial(lpTB[1:NP,1:NT]) <- parm_ifrac_lpTB * parm_init_PPD[i] * parm_ifrac_prevTPT[j] ##late post-TB
+
+## economic states
+initial(CC0) <- 0 #undiscounted cumulative costs
+initial(CC) <- 0 #discounted cumulative costs
+initial(cATTtp) <- 0 #ATT true positive counter
+initial(cATTfp) <- 0 #ATT false positive counter
+initial(cTPT) <- 0   #cumulative TPT counter
 
 ################## dynamics
 ## TODO currently no TPT in
@@ -80,28 +92,36 @@ initial(lpTB[1:NP,1:NT]) <- parm_ifrac_lpTB * parm_init_PPD[i] * parm_ifrac_prev
 ## === TB states
 ## uninfected
 deriv(U[,]) <-  - infections[i,j] - mort*U[i,j] +
-  inflow*frac_U[i,j] + moves_U[i,j]
+  inflow * frac_U[i,j] + moves_U[i,j]
 ## early latent
 deriv(E[,]) <- infections[i,j] + reinfections[i,j] + selfcures[i,j] - stabilizations[i,j] - fastprogs[i,j] - mort*E[i,j] +
-  inflow*frac_E[i,j] + moves_E[i,j]
+  inflow * frac_E[i,j] + moves_E[i,j]
 ## late latent
 deriv(L[,]) <- - Lreinfections[i,j] + stabilizations[i,j] - slowprogs[i,j] - mort*L[i,j] +
-  inflow*frac_L[i,j] + moves_L[i,j]
+  inflow * frac_L[i,j] + moves_L[i,j]
 ## subclinical disease
 deriv(SD[,]) <- fastprogs[i,j] + slowprogs[i,j] - selfcures[i,j]- mort*SD[i,j] - worsens[i,j] +
-  inflow*frac_SD[i,j] + moves_SD[i,j]
+  inflow * frac_SD[i,j] + moves_SD[i,j]
 ## clinical disease
 deriv(CD[,]) <- worsens[i,j]- mort*CD[i,j] + relapses[i,j] +
-  inflow*frac_CD[i,j] + moves_CD[i,j]
+  inflow * frac_CD[i,j] + moves_CD[i,j]
 ## AntiTB treatment
 deriv(ATT[,]) <- detects[i,j] * CD[i,j] - ATT[i,j]/att_time - mort*ATT[i,j] +
-  inflow*frac_ATT[i,j] + moves_ATT[i,j]
+  inflow * frac_ATT[i,j] + moves_ATT[i,j]
 ## early post-TB TODO consider treatment outcomes
 deriv(epTB[,]) <- ATT[i,j]/att_time - relapses[i,j] - epTB[i,j]/late_post_time - mort*epTB[i,j] +
-  inflow*frac_epTB[i,j] + moves_epTB[i,j]
+  inflow * frac_epTB[i,j] + moves_epTB[i,j]
 ## late post-TB
 deriv(lpTB[,]) <- epTB[i,j]/late_post_time - mort*lpTB[i,j] - Preinfections[i,j] +
-  inflow*frac_lpTB[i,j] + moves_lpTB[i,j]
+  inflow * frac_lpTB[i,j] + moves_lpTB[i,j]
+
+## economic states & counters
+deriv(CC0) <- 0
+deriv(CC) <- 0 - disc_rate * CC
+deriv(cATTtp) <- 0 #ATT true positive counter
+deriv(cATTfp) <- 0 #ATT false positive counter
+deriv(cTPT) <- 0   #cumulative TPT counter
+
 
 ## === TB processes TODO write out & parametrize these based on other models
 infections[,] <- 0
@@ -118,25 +138,35 @@ worsens[,] <- 0
 relapses[,] <- 0
 detects[,] <- 0
 
-## === TB split on arrival
-frac_U[2:NP,] <- 0 #uninfected
-frac_E[2:NP,] <- 0 #early latent
-frac_L[2:NP,] <- 0 #late latent
-frac_SD[2:NP,] <- 0 #subclinical disease
-frac_CD[2:NP,] <- 0 #clinical disease
-frac_ATT[2:NP,] <- 0 #AntiTB treatment
-frac_epTB[2:NP,] <- 0 #early post-TB
-frac_lpTB[2:NP,] <- 0 #late post-TB
 
-## these only non-zero
-frac_U[1,] <- parm_frac_U #uninfected
-frac_E[1,] <- parm_frac_E #early latent
-frac_L[1,] <- parm_frac_L #late latent
-frac_SD[1,] <- parm_frac_SD #subclinical disease
-frac_CD[1,] <- parm_frac_CD #clinical disease
-frac_ATT[1,] <- parm_frac_ATT #AntiTB treatment
-frac_epTB[1,] <- parm_frac_epTB #early post-TB
-frac_lpTB[1,] <- parm_frac_lpTB #late post-TB
+
+## screening rates:
+##     attributes Uninfected, LTBI, TB, Previous
+## outcomes:
+##     ATT, TPT, no change
+## probabilities within inflow:
+inflow_toATT_TB <- user(0) #NOTE fp ATT doesn't affect state
+inflow_toATT_P <- user(0) #NOTE fp ATT doesn't affect state - just for counting
+inflow_toATT_rest <- user(0) #NOTE fp ATT doesn't affect state - just for counting
+inflow_toTPT_L <- user(0)    #assume perfect spec
+## vector form for compactness
+inflow_TPTv[1] <- (1-inflow_toTPT_L)
+inflow_TPTv[2] <- (inflow_toTPT_L)
+inflow_TPTv[3] <- 0
+## inflow to remand
+inflow_top[1] <- 1
+inflow_top[2:NP] <- 0
+
+
+## === TB split on arrival, including TPT
+frac_U[,] <- parm_frac_U * inflow_top[i] #uninfected
+frac_E[,] <- parm_frac_E * inflow_top[i] * inflow_TPTv[j] #early latent
+frac_L[,] <- parm_frac_L * inflow_top[i] * inflow_TPTv[j] #late latent
+frac_SD[,] <- parm_frac_SD * inflow_top[i] * (1-inflow_toATT_TB) #subclinical disease
+frac_CD[,] <- parm_frac_CD * inflow_top[i] * (1-inflow_toATT_TB) #clinical disease
+frac_ATT[,] <- (parm_frac_ATT + (parm_frac_SD + parm_frac_CD) * inflow_toATT_TB) * inflow_top[i] #ATT
+frac_epTB[,] <- parm_frac_epTB * inflow_top[i] * inflow_TPTv[j] #early post-TB
+frac_lpTB[,] <- parm_frac_lpTB * inflow_top[i] * inflow_TPTv[j] #late post-TB
 
 
 ## === PPD processes: transitions in 1st index
@@ -174,7 +204,7 @@ moverate[1,5] <- previous_remand
 ## print("moverate 11 :{moverate[1,1]}")
 ## print("moverate 12 :{moverate[1,2]}")
 
-
+## dX_{ij}/dt = \sum_k R_{ik} X_{kj}
 ## matrix multiplications
 moves_Ut[,,] <- moverate[i,j] * U[j,k] #temp
 moves_U[,] <- sum(moves_Ut[i,,j])      #matrix mult
@@ -228,6 +258,9 @@ dim(frac_CD) <- c(NP,NT) #clinical disease
 dim(frac_ATT) <- c(NP,NT) #AntiTB treatment
 dim(frac_epTB) <- c(NP,NT) #early post-TB
 dim(frac_lpTB) <- c(NP,NT) #late post-TB
+
+dim(inflow_TPTv) <- NT
+dim(inflow_top) <- NP
 
 ## PPD transitions
 dim(moverate) <- c(NP,NP)
